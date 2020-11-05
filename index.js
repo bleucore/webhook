f46c8e78adab6ff78aaa43c584269586456e69f4
bleucore <bluecor0@gmail.com>
2020-11-05 07:15:01 +0000

    forth

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-import http from 'http';
-import crypto from 'crypto';
-import { exec } from 'child_process';
-          
+import http from 'http'; import crypto from 'crypto'; import { exec } from 
+'child_process';
+ 
 const SECRET = '123456';
-
-http .createServer((req, res) => { req.on('data', chunk => { const signature =
+ 
+const GITHUB_REPOSITORIES_TO_DIR = { '/home/bitnami/webhook': 
+  '/home/bitnami/webhook2'
+};
+ 
+http .createServer((req, res) => { req.on('data', chunk => { const signature = 
       `sha1=${crypto
         .createHmac('sha1', SECRET) .update(chunk) .digest('hex')}`;
-
+ 
       const isAllowed = req.headers['x-hub-signature'] === signature;
-
+ 
       const body = JSON.parse(chunk);
-	
-      const isMaster = body?.ref === 'refs/heads/master';
-
-      if (isAllowed && isMaster) {
-        // do something
-	console.log('webhook triggered', body);
+ 
+      const isMaster = body?.ref === 'refs/heads/master'; const directory = 
+      GITHUB_REPOSITORIES_TO_DIR[body?.repository?.full_name];
+ 
+      if (isAllowed && isMaster && directory) { try { exec(`cd ${directory} && bash 
+          deploy.sh`);
+        } catch (error) {
+          console.log(error);
+        }
       }
     });
-
+ 
     res.end();
   })
   .listen(8080);
