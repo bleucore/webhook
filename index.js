347ecad29dfd1ede69554eaa785c6a88490a6c91
Debian <bitnami@ip-172-26-11-93.ap-northeast-2.compute.internal>
2020-11-05 06:33:00 +0000

    firts commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import http from 'http';
+import crypto from 'crypto';
+import { exec } from 'child_process';
+
+const SECRET = '123456';
+
+http .createServer((req, res) => { req.on('data', chunk => { const signature =
+      `sha1=${crypto
+        .createHmac('sha1', SECRET) .update(chunk) .digest('hex')}`;
+
+      const isAllowed = req.headers['x-hub-signature'] === signature;
+
+      const body = JSON.parse(chunk);
+
+      const isMaster = body?.ref === 'refs/heads/master';
+
+      if (isAllowed && isMaster) {
+        // do something
+      }
+    });
+
+    res.end();
+  })
+  .listen(8080);
